71aeb37d36618830cabe95f6088560b915aa02fe
huangqijun <Admin_117>
2022-12-27 15:53:10 +0800

    run project

--- user/src/main/java/com/study/kubernete/user/UserApplication.java
+++ user/src/main/java/com/study/kubernete/user/UserApplication.java
@@ -1,9 +1,11 @@
 package com.study.kubernete.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.study.kubernete.user.dao.dbmapper")
 public class UserApplication {
 
     public static void main(String[] args) {
